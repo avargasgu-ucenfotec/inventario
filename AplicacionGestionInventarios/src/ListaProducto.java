2b0f62ceec2f985f223b217373f408ee034126f9
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-09-30 17:06:54 -0600

    Agregar atributos y métodos básicos a clase ListaProducto.

--- AplicacionGestionInventarios/src/ListaProducto.java
+++ AplicacionGestionInventarios/src/ListaProducto.java
@@ -1,2 +1,66 @@
 public class ListaProducto {
+
+    //Clase "ListaProducto" que representa una colección de álbumes de música.
+
+    //Atributos
+    private Producto primero;
+
+    //Métodos
+    //Constructor
+    public ListaProducto(){
+        primero = null;
+    }
+
+    //Getters
+    public Producto getPrimero(){
+        return primero;
+    }
+
+    //Setters
+    public void setPrimero(Producto nuevoPrimero){
+        primero = nuevoPrimero;
+    }
+
+    //Insertar producto al inicio de una lista enlazada simple
+    public void insertarProductoInicio(
+            String tituloProducto,
+            float precioProducto,
+            String generoProducto,
+            String fechaLanzamientoProducto,
+            byte cantidadProducto
+    ){
+        Producto nuevoProducto = new Producto(
+                tituloProducto,
+                precioProducto,
+                generoProducto,
+                fechaLanzamientoProducto,
+                cantidadProducto);
+        nuevoProducto.setSiguiente(primero);
+        setPrimero(nuevoProducto);
+    }
+
+    //Insertar producto al final de la lista enlazada simple
+    public void insertarProductoFinal(
+            String tituloProducto,
+            float precioProducto,
+            String generoProducto,
+            String fechaLanzamientoProducto,
+            byte cantidadProducto
+    ){
+        Producto nuevoProducto = new Producto(
+                tituloProducto,
+                precioProducto,
+                generoProducto,
+                fechaLanzamientoProducto,
+                cantidadProducto);
+        if(primero == null){
+            setPrimero(nuevoProducto);
+            return;
+        }
+        Producto productoTemp = primero;
+        while(productoTemp.getSiguiente() != null){
+            productoTemp = productoTemp.getSiguiente();
+        }
+        productoTemp.setSiguiente(nuevoProducto);
+    }
 }
