b916a31f15ded009fe6e1a9b411b09f51770fc3f
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-10-02 23:10:16 -0600

    Agrega carpeta de imagenes, agrega metodos para imprimir lista de productos.

--- AplicacionGestionInventarios/src/Main.java
+++ AplicacionGestionInventarios/src/Main.java
@@ -3,9 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        ListaProducto miInventario = new ListaProducto();
-
-        miInventario.insertarProductoInicio(
+        //Construir 3 productos
+        Producto album1 = new Producto(
                 "Sgt. Pepper's Lonely Hearts Club Band",
                 10077.1f,
                 "Rock",
@@ -13,7 +12,7 @@ public class Main {
                 10
         );
 
-        miInventario.insertarProductoInicio(
+        Producto album2 = new Producto(
                 "Thriller",
                 19635.0f,
                 "Pop",
@@ -21,7 +20,7 @@ public class Main {
                 15
         );
 
-        miInventario.insertarProductoInicio(
+        Producto album3 = new Producto(
                 "Nevermind",
                 7052.5f,
                 "Grunge",
@@ -29,6 +28,28 @@ public class Main {
                 20
         );
 
+        //Insertar ruta a lista de imágenes de cada álbum
+        album1.setListaImagenes("../img/sgtpepperslonelyheartsclubband_front.jpg");
+        album1.setListaImagenes("../img/sgtpepperslonelyheartsclubband_back.jpg");
+
+        album2.setListaImagenes("../img/thriller_front.jpg");
+        album2.setListaImagenes("../img/thriller_back.jpg");
+
+        album3.setListaImagenes("../img/nevermind_front.jpg");
+        album3.setListaImagenes("../img/nevermind_back.jpg");
+
+        //Construir inventario
+        ListaProducto miInventario = new ListaProducto();
+
+        //Insertar productos al inventario
+        miInventario.insertarProductoInicio(album1);
+        miInventario.insertarProductoInicio(album2);
+        miInventario.insertarProductoInicio(album3);
+
+        //Imprimir contenidos del inventario
+        miInventario.mostrarLista();
+
+        //Imprimir reporte
         miInventario.imprimirReporte();
     }
 }
