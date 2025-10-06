fc08fe5d583efc92291f65098966a3e9f6b14357
sant1agozs <ksant.hernandez@gmail.com>
2025-10-05 18:49:14 -0600

    First Validations.

--- AplicacionGestionInventarios/src/Main.java
+++ AplicacionGestionInventarios/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     static PrintStream out = System.out;
 
     public static void main(String[] args) throws Exception {
-        menu();
+        menu(); // Adiccion del menu
     }
 
     public static void menu() throws Exception {
